bbd89b18e608a2d31dee3f24b05535f2fbda1ed2
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-20 10:59:36 -0500

    initial set up

---
+++ app.js
@@ -0,0 +1,7 @@
+// create manager, engineer, intern, store and push to globalarray
+// choose what type of employee
+// when done, fs
+
+
+
+// other file with function to create cards based on employee type (3) and function to create HTML (include inline or in header css)
---
+++ classes/employee.js
@@ -0,0 +1,13 @@
+class Employee {
+  constructor (name, id, email) {}
+
+  getName()
+  
+  getId()
+  
+  getEmail()
+
+  getRole()
+}
+
+module.exports = Employee
