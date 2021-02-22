206ec637a71716b567d4f3c9e37df70b59001138
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-21 22:19:07 -0500

    working on failing tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,9 @@
-const check = require('../lib/employee')
+const Employee = require('../lib/employee')
+const Employee = require('../lib/employee')
 
-test()
+test('creates a new employee object', () =>{
+  const instanceEmployee = new Employee();
+  expect(instanceEmployee).toBe("object")
+})
+
+// Need to test the getName getID getEmail and getRole functions, and that it can create an employee object
