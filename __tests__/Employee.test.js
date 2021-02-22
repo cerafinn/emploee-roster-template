0ccf0a1d1d28f1f6cc0da8d95b3bbd59ec9f0a5d
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-21 22:27:28 -0500

    working on tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,29 @@
 const Employee = require('../lib/employee')
 const Employee = require('../lib/employee')
 
-test('creates a new employee object', () =>{
+test('creates a new employee object', () => {
   const instanceEmployee = new Employee();
-  expect(instanceEmployee).toBe("object")
+  expect(instanceEmployee).toBe("object");
+});
+
+test('set name through app', () => {
+  const name = 'Anna';
+  const instanceEmployee = new Employee(name);
+  expect(instanceEmployee.name).toBe(name);
+});
+
+test('set id through app', () => {
+  const id = '100';
+  const instanceEmployee = new Employee(id);
+  expect(instanceEmployee.name).toBe(id);
+});
+
+test('set e-mail through app', () => {
+  const email = 'test@example.com';
+  const instanceEmployee = new Employee(email)
 })
 
-// Need to test the getName getID getEmail and getRole functions, and that it can create an employee object
+test('set role through app', () => {
+  const role = 'testData';
+  const instanceEmployee = new Employee(role)
+})
