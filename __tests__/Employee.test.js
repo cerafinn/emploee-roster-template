1bc285bb64e3bca62503c7f9e8f155a7e9473254
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-27 17:10:10 -0500

    working on employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,13 +7,13 @@ test('creates a new employee object', () => {
 
 test('set name through app', () => {
   const name = 'Anna';
-  const instanceEmployee = new Employee(name);
+  const instanceEmployee = new Employee(name, 1, 'test@example.com');
   expect(instanceEmployee.name).toBe(name);
 });
 
 test('set id through app', () => {
   const id = '100';
-  const instanceEmployee = new Employee("Anna", id);
+  const instanceEmployee = new Employee('Anna', id, 'test@example.com');
   expect(instanceEmployee.id).toBe(id);
 });
 
@@ -21,10 +21,28 @@ test('set e-mail through app', () => {
   const email = 'test@example.com';
   const instanceEmployee = new Employee('Anna', 1, email);
   expect(instanceEmployee.email).toBe(email);
-})
+});
 
-test('set role through app', () => {
+test('check that getName gets the name', () => {
+  const name = 'Anna';
+  const instanceEmployee = new Employee(name, 1, 'test@example.com');
+  expect(instanceEmployee.getName()).toBe(name);
+});
+
+test('check that getId gets the id', () => {
+  const id = 100;
+  const instanceEmployee = new Employee('Anna', id, 'test@example.com');
+  expect(instanceEmployee.getId()).toBe(id);
+});
+
+test('check that getEmail gets the email', () => {
+  const email = 'test@example.com';
+  const instanceEmployee = new Employee('Anna', 1, email);
+  expect(instanceEmployee.getEmail()).toBe(email);
+});
+
+test('check that getRole gets the role', () => {
   const role = 'Employee';
   const instanceEmployee = new Employee('Anna', 1, 'test@example.com');
   expect(instanceEmployee.getRole()).toBe(role);
-})
+});
